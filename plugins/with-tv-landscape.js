15a4ed4172a675a135be42c64bd731f5ce124ec2
EREKLE <erekleburjanadze@gmail.com>
2026-09-24 16:20:21 +0400

    Make TV landscape plugin locate generated MainActivity robustly

--- plugins/with-tv-landscape.js
+++ plugins/with-tv-landscape.js
@@ -1,9 +1,23 @@
-const { AndroidConfig, withAndroidManifest } = require('@expo/config-plugins');
+const { withAndroidManifest } = require('@expo/config-plugins');
+
+function isMainIntent(activity) {
+  return (activity?.['intent-filter'] || []).some(filter =>
+    (filter?.action || []).some(action => action?.$?.['android:name'] === 'android.intent.action.MAIN')
+  );
+}
 
 module.exports = function withTvLandscape(config) {
   return withAndroidManifest(config, androidConfig => {
-    const manifest = androidConfig.modResults.manifest;
-    const mainActivity = AndroidConfig.Manifest.getMainActivityOrThrow(manifest);
+    const application = androidConfig.modResults.manifest.application?.[0];
+    const activities = application?.activity || [];
+    const mainActivity = activities.find(activity =>
+      String(activity?.$?.['android:name'] || '').endsWith('MainActivity')
+    ) || activities.find(isMainIntent);
+
+    if (!mainActivity) {
+      throw new Error('MyFilm TV landscape plugin could not locate the Android main activity');
+    }
+
     mainActivity.$ = mainActivity.$ || {};
     mainActivity.$['android:screenOrientation'] = 'landscape';
     return androidConfig;
